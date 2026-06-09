162575cbadf7ee16a187228000577d41b2af7d19
hasanarofid <hasanarofid@gmail.com>
2026-06-09 22:07:31 +0700

    Fix: Convert submodule to normal folder

--- database/seeders/ResearchSeeder.php
+++ database/seeders/ResearchSeeder.php
@@ -31,20 +31,22 @@ class ResearchSeeder extends Seeder
                 $slug = preg_replace('/-+/', '-', trim($slug, '-'));
             }
 
-            \App\Models\Research::create([
-                'title'    => $item['title'],
-                'slug'     => $slug,
-                'subtitle' => $item['subtitle'],
-                'ticker'   => $ticker,
-                'sector'   => $item['sector'],
-                'revenue'  => $item['revenue'],
-                'patmi'    => $item['patmi'],
-                'sales'    => $item['sales'],
-                'price'    => $item['price'],
-                'tags'     => $item['tags'],
-                'date'     => $item['date'],
-                'image'    => $item['image'] ?? null,
-            ]);
+            \App\Models\Research::updateOrCreate(
+                ['slug' => $slug],
+                [
+                    'title'    => $item['title'],
+                    'subtitle' => $item['subtitle'],
+                    'ticker'   => $ticker,
+                    'sector'   => $item['sector'],
+                    'revenue'  => $item['revenue'],
+                    'patmi'    => $item['patmi'],
+                    'sales'    => $item['sales'],
+                    'price'    => $item['price'],
+                    'tags'     => $item['tags'],
+                    'date'     => $item['date'],
+                    'image'    => $item['image'] ?? null,
+                ]
+            );
         }
     }
 }
