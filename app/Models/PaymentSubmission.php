9aa824a2dddbfb2d3e8fe18397c9895acce7142a
hasanarofid <hasanarofid@gmail.com>
2026-06-22 19:33:28 +0700

    perbaikan payment

--- app/Models/PaymentSubmission.php
+++ app/Models/PaymentSubmission.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class PaymentSubmission extends Model
 {
-    //
+    use HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'paket',
+        'durasi_hari',
+        'nominal',
+        'bukti_url',
+        'bukti_path',
+        'status',
+        'submitted_at',
+        'verified_at',
+        'verified_by',
+        'admin_notes',
+        'user_email',
+        'user_nama',
+    ];
 }
